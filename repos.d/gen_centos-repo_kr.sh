f068fa10da1db3a8fba15242e9cbf0609576ec79
Ondrej Famera <ondrej@famera.cz>
2022-02-07 22:37:16 +0900

    Add CentOS 6.x images and new template that allows including extra files that are needed by some of them

--- repos.d/gen_centos-repo_kr.sh
+++ repos.d/gen_centos-repo_kr.sh
@@ -4,6 +4,26 @@ cat <<EOF
 <images>
 EOF
 
+## 6.x BIOS
+sed 's#NAME#centos-6.3#; s#DIST#centos#; s#FNM#6g__centos-6.3#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#139633916#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.4#; s#DIST#centos#; s#FNM#6g__centos-6.4#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#147901176#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.5#; s#DIST#centos#; s#FNM#6g__centos-6.5#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#154986152#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.6#; s#DIST#centos#; s#FNM#6g__centos-6.6#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#165148128#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.7#; s#DIST#centos#; s#FNM#6g__centos-6.7#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#184456172#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.8#; s#DIST#centos#; s#FNM#6g__centos-6.8#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#195386976#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.9#; s#DIST#centos#; s#FNM#6g__centos-6.9#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#199916708#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+sed 's#NAME#centos-6.10#; s#DIST#centos#; s#FNM#6g__centos-6.10#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-6-hacks.sh#; s#XZ_SIZE#190364012#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
+
+## 6.x UEFI
+sed 's#NAME#centos-6.3-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.3-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#135881420#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.3-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.4-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.4-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#147615620#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.4-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.5-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.5-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#155566800#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.5-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.6-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.6-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#167154168#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.6-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.7-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.7-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#187871432#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.7-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.8-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.8-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#241168544#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.8-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.9-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.9-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#198372888#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.9-uefi_VARS.fd#' template_kr_extra.xml
+sed 's#NAME#centos-6.10-uefi#; s#DIST#centos#; s#FNM#6g__centos-6.10-uefi#; s#LXML#centos-6.3-current-uefi.xml#; s#HACK#6g_centos-6-hacks-uefi.sh#; s#XZ_SIZE#189710092#; s#ZST_SIZE#111111111#; s#bios#uefi#; /111111111/d; s#EXTRA#nvram-centos-6.10-uefi_VARS.fd#' template_kr_extra.xml
+
 ## 7.x BIOS
 sed 's#NAME#centos-7.0#; s#DIST#centos#; s#FNM#6g__centos-7.0#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-7-hacks.sh#; s#XZ_SIZE#226559836#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
 sed 's#NAME#centos-7.1#; s#DIST#centos#; s#FNM#6g__centos-7.1#; s#LXML#centos-6.3-current.xml#; s#HACK#6g_centos-7-hacks.sh#; s#XZ_SIZE#244156864#; s#ZST_SIZE#111111111#; /111111111/d' template_kr.xml
